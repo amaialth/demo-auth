a038b8cbe77af89e942e32c15a9bb5d60bf1d8d6
Anbumani <anbuathypathy@gmail.com>
2022-02-17 06:05:40 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyC7HZ2WGEaMMIeg_68fDEfY_GM-RHUZXBw",
-    authDomain: "auth-demo-4e909.firebaseapp.com",
-    projectId: "auth-demo-4e909",
-    storageBucket: "auth-demo-4e909.appspot.com",
-    messagingSenderId: "758786712936",
-    appId: "1:758786712936:web:f2a7c401980ba7e4e6e2dd",
-    measurementId: "G-N1S1YXE10Q"
+    //add yout firebase config here
   },
  
 };
